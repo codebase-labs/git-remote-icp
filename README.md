29b8161b6ff1046eb83c5cc61d2e76797e6fb4fa
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-09-22 09:48:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# git-remote-ic
+
+![](https://img.shields.io/badge/status%EF%B8%8F-experimental-blueviolet)
+
+A Git remote helper for the Internet Computer.
---
+++ flake.nix
@@ -0,0 +1,102 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+    rust-overlay
+  }:
+    let
+      supportedSystems = [
+        flake-utils.lib.system.aarch64-darwin
+        flake-utils.lib.system.x86_64-darwin
+      ];
+    in
+      flake-utils.lib.eachSystem supportedSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (import rust-overlay)
+            ];
+          };
+
+          rust = pkgs.rust-bin.stable.latest.default;
+          # rust = pkgs.rust-bin.nightly."2022-06-01".default;
+
+          # NB: we don't need to overlay our custom toolchain for the *entire*
+          # pkgs (which would require rebuidling anything else which uses rust).
+          # Instead, we just want to update the scope that crane will use by appending
+          # our specific toolchain there.
+          craneLib = (crane.mkLib pkgs).overrideToolchain rust;
+          # craneLib = crane.lib."${system}";
+
+          src = ./.;
+
+          # Build *just* the cargo dependencies, so we can reuse
+          # all of that work (e.g. via cachix) when running in CI
+          cargoArtifacts = craneLib.buildDepsOnly {
+            inherit src;
+            nativeBuildInputs = [
+              pkgs.libiconv
+            ];
+          };
+
+          git-remote-ic = craneLib.buildPackage rec {
+            pname = "git-remote-ic";
+            inherit cargoArtifacts src;
+            nativeBuildInputs = [
+              pkgs.libiconv
+            ];
+          };
+
+          apps = {
+            git-remote-ic = flake-utils.lib.mkApp {
+              drv = git-remote-ic;
+            };
+          };
+        in
+          rec {
+            checks = {
+              inherit git-remote-ic;
+            };
+
+            packages = {
+              inherit git-remote-ic;
+            };
+
+            inherit apps;
+
+            defaultPackage = packages.git-remote-ic;
+            defaultApp = apps.git-remote-ic;
+
+            devShell = pkgs.mkShell {
+              # RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+              RUST_SRC_PATH = pkgs.rust.packages.stable.rustPlatform.rustLibSrc;
+              inputsFrom = builtins.attrValues self.checks;
+              nativeBuildInputs = [
+                rust
+              ];
+            };
+          }
+      );
+}
