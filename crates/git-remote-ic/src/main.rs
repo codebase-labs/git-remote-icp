36c4c1b423ba4367b21f4463f3e6c5b87b75b21a
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-09-22 15:34:34 -0700

    Use Result instead of expect

--- crates/git-remote-ic/src/main.rs
+++ crates/git-remote-ic/src/main.rs
@@ -1,6 +1,6 @@
 #![deny(rust_2018_idioms)]
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use url::Url;
 
 #[derive(Parser)]
@@ -15,23 +15,29 @@ struct Args {
     url: String,
 }
 
+#[derive(Copy, Clone, ValueEnum)]
 enum Commands {
     Capabiliites,
     List,
 }
 
-fn main() {
+fn main() -> Result<(), String> {
     let args = Args::parse();
-    println!("args.repository: {:?}", args.repository);
-    println!("args.url: {:?}", args.url);
+    eprintln!("args.repository: {:?}", args.repository);
+    eprintln!("args.url: {:?}", args.url);
 
     let url = match args.url.strip_prefix("ic::") {
         // Assume ic::<transport>://<address>
-        Some(u) => Url::parse(&u).expect(format!("failed to parse URL: {:#?}", u).as_str()),
+        Some(u) => Url::parse(&u)
+            .map_err(|error| format!("failed to parse URL: {:?}, with error: {:?}", u, error)),
         // Assume ic://<address>
         None => {
-            let u =
-                Url::parse(&args.url).expect(format!("failed to parse URL: {}", args.url).as_str());
+            let u = Url::parse(&args.url).map_err(|error| {
+                format!(
+                    "failed to parse URL: {:?}, with error: {:?}",
+                    args.url, error
+                )
+            })?;
 
             // We want to change the scheme from "ic" to "https" but can't do
             // that with `u.set_scheme("https")` so need to create a new URL
@@ -40,45 +46,51 @@ fn main() {
             //
             // See https://github.com/servo/rust-url/pull/768
             let mut new_url = Url::parse("https://0.0.0.0")
-                .expect(format!("failed to parse URL: {:#?}", args.url).as_str());
+                .map_err(|e| format!("failed to parse URL: {:?}, {}", args.url, e))?;
 
             new_url.set_fragment(u.fragment());
 
             let host = u.host_str();
-            new_url.set_host(host).expect(
-                format!("failed to set host: {:#?}, for URL: {:#?}", host, new_url).as_str(),
-            );
+            new_url.set_host(host).map_err(|error| {
+                format!(
+                    "failed to set host: {:?}, for URL: {:?}, with error: {}",
+                    host, new_url, error
+                )
+            })?;
 
             let password = u.password();
-            new_url.set_password(password).expect(
+            new_url.set_password(password).map_err(|error| {
                 format!(
-                    "failed to set password: {:#?}, for URL: {:#?}",
-                    password, new_url
+                    "failed to set password: {:?}, for URL: {:?}, with error: {:?}",
+                    password, new_url, error
                 )
-                .as_str(),
-            );
+            })?;
 
             new_url.set_path(u.path());
 
             let port = u.port();
-            new_url.set_port(port).expect(
-                format!("failed to set port: {:#?}, for URL: {:#?}", port, new_url).as_str(),
-            );
+            new_url.set_port(port).map_err(|error| {
+                format!(
+                    "failed to set port: {:?}, for URL: {:?}, with error: {:?}",
+                    port, new_url, error
+                )
+            })?;
 
             new_url.set_query(u.query());
 
             let username = u.username();
-            new_url.set_username(username).expect(
+            new_url.set_username(username).map_err(|error| {
                 format!(
-                    "failed to set username: {:#?}, for URL: {:#?}",
-                    username, new_url
+                    "failed to set username: {:?}, for URL: {:?}, with error: {:?}",
+                    username, new_url, error
                 )
-                .as_str(),
-            );
+            })?;
 
-            new_url
+            Ok(new_url)
         }
-    };
+    }?;
 
-    println!("url: {}", url);
+    eprintln!("url: {}", url);
+
+    Ok(())
 }
