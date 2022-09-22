14396158e793e84c177177698a1b5a4be206fc3c
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-09-22 13:50:46 -0700

    [WIP] Use clap

--- crates/git-remote-ic/src/main.rs
+++ crates/git-remote-ic/src/main.rs
@@ -1,3 +1,26 @@
 #![deny(rust_2018_idioms)]
 
-fn main() {}
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(about, version)]
+struct Args {
+    /// A remote repository; either the name of a configured remote or a URL
+    #[clap(value_parser)]
+    repository: String,
+
+    /// A URL of the form ic://<address> or ic::<transport>://<address>
+    #[clap(value_parser)]
+    url: Option<String>,
+}
+
+enum Commands {
+    Capabiliites,
+    List,
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("repository: {:?}", args.repository);
+    println!("url: {:?}", args.url);
+}
