f1c3662e28f4ccd8cc4f9c7de9c754e7f5a56c2c
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-09-22 17:57:16 -0700

    Fix URL handling

--- crates/git-remote-ic/src/main.rs
+++ crates/git-remote-ic/src/main.rs
@@ -1,7 +1,6 @@
 #![deny(rust_2018_idioms)]
 
 use clap::{Command, FromArgMatches as _, Parser, Subcommand as _, ValueEnum };
-use url::Url;
 
 #[derive(Parser)]
 #[clap(about, version)]
@@ -36,69 +35,14 @@ fn main() -> Result<(), String> {
     eprintln!("cli.repository: {:?}", cli.repository);
     eprintln!("cli.url: {:?}", cli.url);
 
-    let url = match cli.url.strip_prefix("ic::") {
-        // Assume ic::<transport>://<address>
-        Some(u) => Url::parse(&u)
-            .map_err(|error| format!("failed to parse URL: {:?}, with error: {:?}", u, error)),
-        // Assume ic://<address>
-        None => {
-            let u = Url::parse(&cli.url).map_err(|error| {
-                format!(
-                    "failed to parse URL: {:?}, with error: {:?}",
-                    cli.url, error
-                )
-            })?;
-
-            // We want to change the scheme from "ic" to "https" but can't do
-            // that with `u.set_scheme("https")` so need to create a new URL
-            // that already has that scheme and change the other parts of the
-            // URL instead.
-            //
-            // See https://github.com/servo/rust-url/pull/768
-            let mut new_url = Url::parse("https://0.0.0.0")
-                .map_err(|e| format!("failed to parse URL: {:?}, {}", cli.url, e))?;
-
-            new_url.set_fragment(u.fragment());
-
-            let host = u.host_str();
-            new_url.set_host(host).map_err(|error| {
-                format!(
-                    "failed to set host: {:?}, for URL: {:?}, with error: {}",
-                    host, new_url, error
-                )
-            })?;
-
-            let password = u.password();
-            new_url.set_password(password).map_err(|error| {
-                format!(
-                    "failed to set password: {:?}, for URL: {:?}, with error: {:?}",
-                    password, new_url, error
-                )
-            })?;
-
-            new_url.set_path(u.path());
-
-            let port = u.port();
-            new_url.set_port(port).map_err(|error| {
-                format!(
-                    "failed to set port: {:?}, for URL: {:?}, with error: {:?}",
-                    port, new_url, error
-                )
-            })?;
-
-            new_url.set_query(u.query());
-
-            let username = u.username();
-            new_url.set_username(username).map_err(|error| {
-                format!(
-                    "failed to set username: {:?}, for URL: {:?}, with error: {:?}",
-                    username, new_url, error
-                )
-            })?;
-
-            Ok(new_url)
-        }
-    }?;
+    let url: String = match cli.url.strip_prefix("ic://") {
+        // The supplied URL was of the form `ic://<address>` so we change it to
+        // `https://<address>`
+        Some(address) => format!("https://{}", address),
+        // The supplied url was of the form `ic::<transport>://<address>` but
+        // Git invoked the remote helper with `<transport>://<address>`
+        None => cli.url.to_string(),
+    };
 
     eprintln!("url: {}", url);
 
