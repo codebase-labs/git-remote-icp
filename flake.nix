592d065a515624eedfd75ac2681ef132c70cf415
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-10-25 13:13:41 -0700

    Add missing capabilities

--- flake.nix
+++ flake.nix
@@ -116,10 +116,9 @@
             node_modules_attrs = {
               sourceOverrides = {
                 git-http-backend = sourceInfo: drv: drv.overrideAttrs (old: {
-                  patches = builtins.toFile "git-http-backend.patch"
-                    ''
+                  patches = builtins.toFile "git-http-backend.patch" ''
 diff --git a/lib/service.js b/lib/service.js
-index 168b68e..d9f6053 100644
+index 168b68e..73ceade 100644
 --- a/lib/service.js
 +++ b/lib/service.js
 @@ -102,5 +102,5 @@ function infoPrelude (service) {
@@ -127,7 +126,7 @@ index 168b68e..d9f6053 100644
          return Array(4 - n.length + 1).join('0') + n + s;
      }
 -    return pack('# service=' + service + '\n') + '0000';
-+    return pack('# service=' + service + '\n') + '0000' + pack('version 2\n') + '0000';
++    return pack('# service=' + service + '\n') + '0000' + pack('version 2\n') + pack('fetch\n') + pack('ls-refs\n') + '0000';
  }
 
                     '';
