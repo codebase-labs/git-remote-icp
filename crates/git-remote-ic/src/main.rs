dfe76abf3db8442834b4a7567bc3626fbe70a8de
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-11-03 16:25:24 -0700

    Set signature components

--- crates/git-remote-ic/src/main.rs
+++ crates/git-remote-ic/src/main.rs
@@ -92,12 +92,21 @@ async fn main() -> anyhow::Result<()> {
         .lock()
         .expect("failed to obtain lock on signing config");
 
-    // new_signing_config.set_components(&signing_components);
     let private_key = http_sig::EcdsaP256Sha256Sign::new_pkcs8_pem(&private_key_data)
         .expect("failed to create private key from key material");
 
     // TODO: read ic.keyId from git config
     let mut new_signing_config = SigningConfig::new("sig", "key_id", private_key);
+
+    let signature_components = vec![
+        http_sig::SignatureComponent::Header(http::header::CONTENT_LENGTH),
+        http_sig::SignatureComponent::Header(http::header::CONTENT_TYPE),
+        http_sig::SignatureComponent::Header(http::header::DATE),
+        http_sig::SignatureComponent::Header(http::header::HeaderName::from_static("digest")),
+        http_sig::SignatureComponent::Header(http::header::HOST),
+    ];
+
+    new_signing_config.set_components(&signature_components);
     new_signing_config.set_signature_created_auto();
     new_signing_config.set_signature_expires_relative(60000); // 1 minute
 
