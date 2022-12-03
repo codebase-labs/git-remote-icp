67d948e3af4e530862d08aac0a57766ecb4d9855
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-12-02 20:06:23 -0800

    [WIP] Test parsers that take readers, add fixtures

--- crates/git-remote-icp/src/report_status_v2/mod.rs
+++ crates/git-remote-icp/src/report_status_v2/mod.rs
@@ -303,7 +303,54 @@ fn as_slice(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use git::bstr::ByteSlice;
+    use async_trait::async_trait;
+    use core::pin::Pin;
+    use git::bstr::{BStr, ByteSlice};
+
+    struct Fixture<'a>(&'a [u8]);
+
+    impl<'a> Fixture<'a> {
+        fn project(self: Pin<&mut Self>) -> Pin<&mut &'a [u8]> {
+            unsafe { Pin::new(&mut self.get_unchecked_mut().0) }
+        }
+    }
+
+    impl<'a> git::protocol::futures_io::AsyncRead for Fixture<'a> {
+        fn poll_read(
+            self: std::pin::Pin<&mut Self>,
+            cx: &mut std::task::Context<'_>,
+            buf: &mut [u8],
+        ) -> std::task::Poll<std::io::Result<usize>> {
+            self.project().poll_read(cx, buf)
+        }
+    }
+
+    impl<'a> git::protocol::futures_io::AsyncBufRead for Fixture<'a> {
+        fn poll_fill_buf(
+            self: std::pin::Pin<&mut Self>,
+            cx: &mut std::task::Context<'_>,
+        ) -> std::task::Poll<std::io::Result<&[u8]>> {
+            self.project().poll_fill_buf(cx)
+        }
+
+        fn consume(self: std::pin::Pin<&mut Self>, amt: usize) {
+            self.project().consume(amt)
+        }
+    }
+
+    #[async_trait(?Send)]
+    impl<'a> git::protocol::transport::client::ReadlineBufRead for Fixture<'a> {
+        async fn readline(
+            &mut self,
+        ) -> Option<std::io::Result<Result<packetline::PacketLineRef<'_>, packetline::decode::Error>>>
+        {
+            let bytes: &BStr = self.0.into();
+            let mut lines = bytes.lines();
+            let res = lines.next()?;
+            self.0 = lines.as_bytes();
+            Some(Ok(Ok(packetline::PacketLineRef::Data(res))))
+        }
+    }
 
     #[test]
     fn test_read_and_parse() {
@@ -381,39 +428,34 @@ mod tests {
         )
     }
 
-    /*
-    #[test]
-    fn test_read_and_parse_command_status_v2_ok() {
-        let input = b"0016ok refs/heads/main"
-        let result = parse_command_status_v2::<nom::error::Error<_>>(input);
+    #[tokio::test]
+    async fn test_read_and_parse_command_ok_v2_option_lines_0() {
+        let input = b"0016ok refs/heads/main";
+        let mut reader = Fixture(input);
+        let result = read_and_parse_command_ok_v2::<nom::error::Error<_>>(&mut reader).await;
         assert_eq!(
-            result.map(|x| x.1),
+            result,
             Ok(CommandStatusV2::Ok(
                 RefName(BString::new(b"refs/heads/main".to_vec())),
                 Vec::new(),
             )),
-            "command-status-v2"
+            "command-ok-v2"
         )
     }
 
-    #[test]
-    fn test_read_and_parse_command_status_v2_ok_newline() {
+    #[tokio::test]
+    async fn test_read_and_parse_command_ok_v2_option_lines_0_newline() {
         let input = b"0017ok refs/heads/main\n";
-        let result = parse_command_status_v2::<nom::error::Error<_>>(input);
+        let mut reader = Fixture(input);
+        let result = read_and_parse_command_ok_v2::<nom::error::Error<_>>(&mut reader).await;
         assert_eq!(
-            result.map(|x| x.1),
+            result,
             Ok(CommandStatusV2::Ok(
                 RefName(BString::new(b"refs/heads/main".to_vec())),
                 Vec::new(),
             )),
-            "command-status-v2"
+            "command-ok-v2"
         )
-    }
-    */
-
-    #[test]
-    fn test_read_and_parse_command_ok_v2_option_lines_0() {
-        assert!(false)
     }
 
     #[test]
