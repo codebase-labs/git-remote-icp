c7c6659d5e9bf10e6172c6f4101a50af3ea0d051
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-12-08 23:08:40 -0800

    Only set actual version once its known

--- crates/git-remote-icp/src/git/transport/client/icp/connection.rs
+++ crates/git-remote-icp/src/git/transport/client/icp/connection.rs
@@ -18,7 +18,7 @@ pub struct Connection {
     pub user_agent_header: HeaderField,
     // pub supported_versions: [Protocol; 1],
     pub desired_version: transport::Protocol,
-    pub actual_version: transport::Protocol,
+    pub actual_version: Option<transport::Protocol>,
     pub service: Option<transport::Service>,
 }
 
@@ -61,8 +61,7 @@ impl Connection {
             // TODO: Protocol::V2?
             // supported_versions: [desired_version],
             desired_version,
-            // TODO: None?
-            actual_version: desired_version,
+            actual_version: None,
             service: None,
         };
 
