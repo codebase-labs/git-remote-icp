7da6def98c82e6cb1959648cbffb4729d4d59717
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-12-19 17:19:08 -0800

    Add missed configuration changes

--- crates/git-remote-icp/src/main.rs
+++ crates/git-remote-icp/src/main.rs
@@ -16,12 +16,7 @@ use ic_agent::identity::{Identity, Secp256k1Identity};
 pub async fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    // TODO: consider falling back to AnonymousIdentity if icp.privateKey isn't
-    // set to allow users to clone from public repos using the icp:// scheme.
-    let private_key_path = config::private_key().map_err(|_| {
-        anyhow!("failed to read icp.privateKey from git config. Set with `git config --global icp.privateKey <path to private key>`")
-    })?;
-
+    let private_key_path = config::private_key()?;
     trace!("private key path: {}", private_key_path);
 
     let identity = Secp256k1Identity::from_pem_file(private_key_path)?;
@@ -31,13 +26,13 @@ pub async fn main() -> anyhow::Result<()> {
     trace!("principal: {}", principal);
 
     let fetch_root_key = config::fetch_root_key();
-    trace!("fetch_root_key: {}", fetch_root_key);
+    trace!("fetch root key: {}", fetch_root_key);
 
     let replica_url = config::replica_url();
-    trace!("replica_url: {}", replica_url);
+    trace!("replica url: {}", replica_url);
 
     let canister_id = config::canister_id()?;
-    trace!("canister_id: {}", canister_id);
+    trace!("canister id: {}", canister_id);
 
     let connect = connect::make::<String, _>(identity, fetch_root_key, &replica_url, canister_id);
 
